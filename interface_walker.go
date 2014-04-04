1bb4a64699df5601c372bb79d9be61d07cdeda38
duwei <duv123@gmail.com>
2014-04-04 21:07:34 +0800

    all function return bool has

--- interface_walker.go
+++ interface_walker.go
@@ -1,5 +1,7 @@
 package goutils
-
+/**
+*toolkit for the interface{}
+*/
 import (
     "fmt"
     "strconv"
@@ -18,17 +20,8 @@ func NewInterfaceWalker(obj interface{}) *Object {
 }
 
 
-func (obj *Object) GetInterface(path interface{}) (has bool,val interface{}) {
-    path_str:=os_path.Clean(fmt.Sprint(path))
-    if path_str != "." && path_str[0] != '/' {
-        path_str = strings.Join(obj.paths, "/") + "/" + path_str
-    }
-    has, val= InterfaceWalk(obj.data, path_str)
-    return
-}
-
-func (obj *Object) GetObject(path interface{}) (has bool,val *Object) {
-    has, tmp:= obj.GetInterface(path)
+func (obj *Object) GetObject(path interface{}) (val *Object,has bool) {
+     tmp,has:= obj.GetInterface(path)
     if has{
        val=NewInterfaceWalker(tmp)
      }
@@ -41,139 +34,143 @@ func (obj *Object) Gointo(path interface{}) {
     obj.paths=strings.Split(path_str_clean,"/")
 }
 
-/*
-*读取指定项的值
- */
-func (obj *Object) GetString(path interface{}, def ...string) string {
-    has,val := obj.GetInterface(path)
+
+func (obj *Object) GetInterface(path interface{}) (val interface{},has bool) {
+    path_str:=os_path.Clean(fmt.Sprint(path))
+    if path_str != "." && path_str[0] != '/' {
+        path_str = strings.Join(obj.paths, "/") + "/" + path_str
+    }
+    val,has= InterfaceWalk(obj.data, path_str)
+    return
+}
+
+
+func (obj *Object)GetInterfaceSlice(path interface{},def ...[]interface{})(val []interface{},has bool){
+   tmp,has := obj.GetInterface(path)
     if has{
-	   return fmt.Sprint(val)
+        _type:=reflect.TypeOf(tmp).String()
+        if len(_type)<3 || _type[:2]!="[]"{
+         log.Printf("GetStringArray failed,[%s] not slice",_type)
+       }else{
+          _value:=reflect.ValueOf(tmp)
+          val_len:=_value.Len()
+	       result := make([]interface{}, val_len)
+	       for i:=0;i<val_len;i++{
+	         result[i]=_value.Index(i).Interface()
+	         }
+	         return result,true
+         }
     }
     if len(def) > 0 {
-        return def[0]
+        return def[0],false
     } else {
-        return ""
+        return []interface{}{},false
     }
 }
 
-func (obj *Object) GetStringSlice(path interface{}, def ...[]string) []string {
-   interface_array:=obj.GetInterfaceSlice(path)
-   if(len(interface_array)>0){
+
+/*
+*读取指定项的值
+ */
+func (obj *Object) GetString(path interface{}, def ...string) (val string,has bool) {
+    tmp,has:= obj.GetInterface(path)
+    if has{
+	   return fmt.Sprint(tmp),true
+    }
+    if len(def) > 0 {
+        return def[0],false
+    } else {
+        return "",false
+    }
+}
+
+func (obj *Object) GetStringSlice(path interface{}, def ...[]string)(val []string,has bool) {
+   interface_array,has:=obj.GetInterfaceSlice(path)
+   if(has){
 	   result:=make([]string,len(interface_array))
 	   for i,v:=range interface_array{
 	      result[i]=fmt.Sprint(v)
 	    }
-	   return result
+	   return result,true
    }else{
       if (len(def)>0){
-        return def[0]
+        return def[0],false
       }else{
-       return []string{}
+       return []string{},false
       }
    }
 }
 
-func (obj *Object)GetInterfaceSlice(path interface{},def ...[]interface{}) []interface{}{
-   has,val := obj.GetInterface(path)
-    if has{
-        _type:=reflect.TypeOf(val).String()
-        if len(_type)<3 || _type[:2]!="[]"{
-         log.Printf("GetStringArray failed,[%s] not slice",_type)
-       }else{
-          value_of_t:=reflect.ValueOf(val)
-          val_len:=value_of_t.Len()
-	       result := make([]interface{}, val_len)
-	       for i:=0;i<val_len;i++{
-	         result[i]=value_of_t.Index(i).Interface()
-	         }
-	         return result
-           
-         }
-    }
-    if len(def) > 0 {
-        return def[0]
-    } else {
-        return []interface{}{}
-    }
-}
 
-func (obj *Object) GetInt(path interface{}, def ...int) int {
-    val := obj.GetFloat(path)
-    if val!=float64(-1){
-       return int(val)
+func (obj *Object) GetInt(path interface{}, def ...int) (val int,has bool) {
+     tmp,has := obj.GetFloat(path)
+    if has{
+       return int(tmp),true
      }else{
 	    if len(def) > 0 {
-	        return def[0]
+	        return def[0],false
 	    } else {
-	        return -1
+	        return -1,false
 	    }
      }
 }
 
-func (obj *Object) GetIntSlice(path interface{}, def ...[]int) []int {
-    float_arr := obj.GetFloatSlice(path)
-    if len(float_arr)>0{
+func (obj *Object) GetIntSlice(path interface{}, def ...[]int)(val []int,has bool) {
+    float_arr,has := obj.GetFloatSlice(path)
+    if has{
 	    result := make([]int, len(float_arr))
 	    for i, v := range float_arr {
 	        result[i] = int(v)
 	    }
-	    return result
+	    return result,true
     }else{
     if len(def) > 0 {
-        return def[0]
+        return def[0],false
     } else {
-        return []int{}
+        return []int{},false
     }
     }
 }
 
-func intArr2Float(intArr []int) []float64 {
-    floatArr := make([]float64, len(intArr))
-    for index, v := range intArr {
-        floatArr[index] = float64(v)
-    }
-    return floatArr
-}
-
-func (obj *Object) GetFloat(path interface{}, def ...float64) float64 {
-    str:=obj.GetString(path)
-    if(str!=""){
+func (obj *Object) GetFloat(path interface{}, def ...float64)(val float64,has bool) {
+    str,has:=obj.GetString(path)
+    if(has){
 	    ret, err := strconv.ParseFloat(str, 64)
 	    if err != nil {
 	        log.Printf("GetFloat faild [%s] value:[%v]",path,ret)
-	        return -1
+	        return -1,false
 	    }
-      return ret
+      return ret,true
     }else{
        if len(def) > 0 {
-        return def[0]
+        return def[0],false
 	    } else {
-	     return -1
+	     return -1,false
 	    }
     }
 }
 
-func (obj *Object) GetFloatSlice(path interface{}, def ...[]float64) []float64 {
-    str_arr := obj.GetStringSlice(path)
-    if len(str_arr)>0{
+func (obj *Object) GetFloatSlice(path interface{}, def ...[]float64)(val []float64 ,has bool){
+    str_arr,has := obj.GetStringSlice(path)
+    if has {
 	    result := make([]float64, len(str_arr))
 	    for i, v := range str_arr {
 	        ret, _ := strconv.ParseFloat(v, 64)
 	        result[i] = ret
 	    }
-	    return result
+	    return result,true
     }else{
 	     if len(def) > 0 {
-	        return def[0]
+	        return def[0],false
 	    } else {
-	       return []float64{}
+	       return []float64{},false
 	    }
     }
 }
 
 func (obj *Object) GetBool(path interface{}) bool {
-    val := obj.GetString(path)
-    if val == "" {
+    val,has:= obj.GetString(path)
+    if !has {
         return false
     }
     bv, err := strconv.ParseBool(val)
@@ -186,11 +183,11 @@ func (obj *Object) GetBool(path interface{}) bool {
 /**
 *quick get the val from a interface
 */
-func InterfaceWalk(obj interface{}, path interface{}) (has bool, val interface{}) {
+func InterfaceWalk(obj interface{}, path interface{}) (val interface{},has bool) {
     path_str:= strings.TrimSpace(os_path.Clean(fmt.Sprint(path)))
 //    fmt.Println("path:",path_str)
     if path_str == "/" || path_str == "." {
-        return true, obj
+        return  obj,true
     }
     val_tmp:= obj
     paths := strings.Split(strings.Trim(path_str, "/"), "/")
@@ -233,7 +230,7 @@ func InterfaceWalk(obj interface{}, path interface{}) (has bool, val interface{}
        n++
     }
     if n==len(paths){
-   	 return true,val_tmp
+   	 return val_tmp,true
      }
-    return false,nil
+    return nil,false
 }
--- interface_walker_test.go
+++ interface_walker_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestGetVal(t *testing.T) {
    if (3>2){
-	    str := `{"a":{"c":1,"f":1.1},"b":[1,2],"d":{"1":{"a":"ccc"},"2":3}}`
+	    str := `{"a":{"c":1,"f":1.1},"b":[1,2],"d":{"1":{"a":"ccc"},"2":3},"e":[]}`
 	    var m map[string]interface{}
 	    err := json.Unmarshal([]byte(str), &m)
 	    assert.Equal(t, err, nil)
@@ -21,9 +21,13 @@ func TestGetVal(t *testing.T) {
 	    cases["d//1/a"] = "ccc"
 	    for k, v := range cases {
 //	    fmt.Println(k,v, w.GetString(k))
-	        assert.Equal(t, v, w.GetString(k))
+			val,_:=w.GetString(k)
+	        assert.Equal(t, v,val )
 	    }
-	   assert.Equal(t, w.GetIntSlice("b"),[]int{1,2})
+	    val,_:=w.GetIntSlice("b")
+	    assert.Equal(t,val,[]int{1,2})
+	    val_e,_:=w.GetIntSlice("e")
+	    assert.Equal(t,val_e,[]int{})
     }
     
     int_map:=make(map[int]int)
@@ -32,12 +36,15 @@ func TestGetVal(t *testing.T) {
     slice_walker:=NewInterfaceWalker(int_map)
     for k,v := range int_map {
 //       fmt.Println("int_map:",k,"->",v, "?=",slice_walker.GetInt(k))
-       assert.Equal(t, v,slice_walker.GetInt(k))
+			val,_:=slice_walker.GetInt(k)
+       assert.Equal(t, v,val)
      }
      arr:=[]int{1,3,5}
      walker_3:=NewInterfaceWalker(arr) 
      for i,v:=range arr{
-        assert.Equal(t,v, walker_3.GetInt(i))
+         val,_:=walker_3.GetInt(i)
+        assert.Equal(t,v, val)
      }  
-        assert.Equal(t,arr, walker_3.GetIntSlice(""))
+     val,_:=walker_3.GetIntSlice("")
+    assert.Equal(t,arr,val )
 }
