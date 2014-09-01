0ced7e9615be3e382e72285cad9b229322007737
duwei <duv123@gmail.com>
2014-09-01 21:20:41 +0800

    fmt

--- cache/none.go
+++ cache/none.go
@@ -1,28 +1,29 @@
 package cache
 
-type NoneCache struct{
-    Cache
-}
-func NewNoneCache() *NoneCache{
-  return &NoneCache{}
-}
-func (cache *NoneCache)Set(key string,val []byte,life int64) bool{
-  return true
+type NoneCache struct {
+	Cache
 }
 
-func (cache *NoneCache)Get(key string)(has bool,data []byte){
-   return false,nil
+func NewNoneCache() *NoneCache {
+	return &NoneCache{}
 }
-func (cache *NoneCache)Delete(key string)(suc bool){
-    return true
-}
-
-func (cache *NoneCache)DeleteAll()(suc bool){
-    return true
+func (cache *NoneCache) Set(key string, val []byte, life int64) bool {
+	return true
 }
 
-func (cache *NoneCache)GC(){
+func (cache *NoneCache) Get(key string) (has bool, data []byte) {
+	return false, nil
+}
+func (cache *NoneCache) Delete(key string) (suc bool) {
+	return true
+}
+
+func (cache *NoneCache) DeleteAll() (suc bool) {
+	return true
+}
+
+func (cache *NoneCache) GC() {
 
 }
-func (cache *NoneCache)StartGcTimer(sec int64){
+func (cache *NoneCache) StartGcTimer(sec int64) {
 }
--- slice_test.go
+++ slice_test.go
@@ -1,15 +1,15 @@
 package utils
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 func TestIsInArray(t *testing.T) {
-  arr:=[]string{"a","b","c"}
-  a(arr)
+	arr := []string{"a", "b", "c"}
+	a(arr)
 }
 
-func a(b interface{}){
-  fmt.Println(b)
+func a(b interface{}) {
+	fmt.Println(b)
 }
